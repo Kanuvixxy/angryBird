bceee51f05694a361b49598e454fe9d27b849d1a
Vihaan Kharia <vihaankharia8@gmail.com>
2020-09-24 16:49:53 +0530

    Update Ground.js

--- Ground.js
+++ Ground.js
@@ -6,12 +6,11 @@ class Ground {
       this.body = Bodies.rectangle(x,y,width,height,options);
       this.width = width;
       this.height = height;
-      this.image = loadImage("sprites/ground.png");
       World.add(world, this.body);
     }
     display(){
       var pos =this.body.position;
-      imageMode(CENTER);
-      image(this.image, pos.x, pos.y, this.width, this.height);
+      rectMode(CENTER);
+      rect(pos.x, pos.y, this.width, this.height);
     }
   };
